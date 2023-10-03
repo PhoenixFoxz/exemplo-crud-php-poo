cd4f605ce68abe9095b3056d60f543b2a7eb6168
Felipe Moura Martins <felipemouramartins3@gmail.com>
2023-10-03 11:52:24 -0300

    Classe banco com o método conecta

---
+++ src/Fabricante.php
@@ -0,0 +1,35 @@
+<?php 
+namespace ExemploCrudPoo;
+use PDO;
+final class Fabricante {
+    private int $id;
+    private string $nome;
+
+    // Esta propriedade receberá os recursos PDO através da classe Banco (dependência desse projeto)
+    private PDO $conexao;
+
+    public function __construct()
+    {
+        // No momento em que um objeto Fabricante for criado, automaticamente será feita a chamada do método "conecta" existente na classe Banco.
+        $this->conexao = Banco::conecta();
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function setId(int $id): self {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self {
+        $this->nome = $nome;
+        return $this;
+    }
+}
+?>
